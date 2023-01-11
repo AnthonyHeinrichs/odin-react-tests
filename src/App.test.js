6e65494f985545d89d6e21f17fa8e04aef2d9816
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2023-01-11 16:06:10 +0100

    Test for correct heading

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import App from "./App";
+
+describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch(/Hello test app!/i);
+  });
+});
